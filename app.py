b51d998c60e21b76c2b6c117a54bb06961ba423d
cloudpunchx <siobhanbonardi@hotmail.com>
2023-01-28 20:46:25 -0700

    Finished the first function to verify username/password combo and return.

--- app.py
+++ app.py
@@ -11,8 +11,21 @@ conn = mariadb.connect(
 cursor = conn.cursor()
 
 # Function to check if username/password combo exists
-def verify_user(username, password):
+def verify_user():
+    # get user input for username and password, save in variables
     username = input("Please Enter Your Username: ")
     password = input("Please Enter Your Password: ")
-    cursor.execute("SELECT username AND password FROM client")
+    # the cursor executes statements to database, selecting username & password combo(%s = placeholder), passed client input as a tuple
+    cursor.execute("SELECT username, password FROM client WHERE username = %s AND password = %s", (username, password))
+    # the cursor is fetching all rows from above statement, storing in result
+    result = cursor.fetchall()
+    print(result)
 
+# Function to create post
+def create_post(client_id):
+    title = input("Post title: ")
+    content = input("Post Content: ")
+    cursor.execute("CALL create_post(?, ?)", [content, title])
+    conn.commit()
+
+verify_user()
