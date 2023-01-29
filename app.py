2f577e464e9b254f0a01c428fd516408541007c2
cloudpunchx <siobhanbonardi@hotmail.com>
2023-01-28 19:00:39 -0700

    Created gitignore and dbcreds.

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 import mariadb
+import dbcreds
+
+conn = mariadb.connect(
+                        user = dbcreds.user,
+                        password = dbcreds.password,
+                        host = dbcreds.host,
+                        port = dbcreds.port,
+                        database = dbcreds.database
+                        )
+cursor = conn.cursor()
+
+# Function to check if username/password combo exists
+def verify_user(username, password):
+    username = input("Please Enter Your Username: ")
+    password = input("Please Enter Your Password: ")
+    cursor.execute("SELECT username AND password FROM client")
+
